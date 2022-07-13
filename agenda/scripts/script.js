a154b193e42fa36f06b82f2ac69077f0e190dd92
Angel Solano <solanoangel@icloud.com>
2022-07-13 07:07:36 -0700

    scheduler.html stylished

---
+++ agenda/scripts/script.js
@@ -0,0 +1,41 @@
+// Get the modal
+var modal = document.getElementById('id01');
+var user = "asolharo"
+var pwd = "testing"
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function login(){
+    console.clear();
+    //document.getElementById('welcomeDiv').style.display = "none";
+    modal.style.display='block'
+}
+
+function goToScheduler() {
+    console.clear();
+    var uname = document.getElementById('name').value;
+    var pass = document.getElementById('pass').value;
+    sessionStorage.setItem(uname, pass);
+
+    if (uname == user & pass == pwd) {
+        console.log("It's working")
+        //document.getElementById('welcomeDiv').style.display = "block";
+        //alert( "username = " + sessionStorage.getItem(uname));
+        window.location.href = './scheduler.html';
+        modal.style.display = "none";
+    }
+
+    else {
+        alert("Login failed. Please check again:\nUsername\nPassword");
+    }
+
+
+    console.log(uname)
+    console.log(pass)
+
+}
